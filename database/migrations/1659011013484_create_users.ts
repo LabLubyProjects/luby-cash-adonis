59d8a0b975eaccfec548f5a60cfe1b74d2223fcb
Breno Vergilio <bjvergilio47@gmail.com>
2022-07-28 08:53:41 -0400

    ✨ creating database tables with migrations

---
+++ database/migrations/1659011013484_create_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('full_name', 50).notNullable()
+      table.string('cpf_number', 14).unique().notNullable()
+      table.string('email', 50).unique().notNullable()
+      table.string('password').notNullable()
+      table.string('password_recover_token', 40)
+      table.timestamp('password_recover_token_duration', { useTz: true })
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1659012476971_create_pix_transactions.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'pix_transactions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table
+        .uuid('source_user_id')
+        .references('id')
+        .inTable('users')
+        .notNullable()
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table
+        .uuid('target_user_id')
+        .references('id')
+        .inTable('users')
+        .notNullable()
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
