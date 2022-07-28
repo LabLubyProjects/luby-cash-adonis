5b77a6a2f643611389b49e65d93c9ca28e0f2a0a
Breno Vergilio <bjvergilio47@gmail.com>
2022-07-28 10:34:39 -0400

    ✨ adding authorization middleware and setting up error handler

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,20 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract) {
+    if (error.code === 'E_UNAUTHORIZED_ACCESS')
+      return ctx.response.unauthorized({ statusCode: 401, message: 'Invalid credentials' })
+    if (error.code === 'E_ROUTE_NOT_FOUND')
+      return ctx.response.badRequest({ statusCode: 404, message: 'Route not found' })
+    if (error.code === 'E_VALIDATION_FAILURE') return ctx.response.status(422).send(error.messages)
+
+    return ctx.response.internalServerError({ statusCode: 500, message: 'Internal Server Error' })
   }
 }
