4d26287871769623f369cebb284743252089beb0
Breno Vergilio <bjvergilio47@gmail.com>
2022-08-05 00:30:18 -0400

    🎨 reorganizing routes

--- start/routes.ts
+++ start/routes.ts
@@ -16,18 +16,19 @@ Route.group(() => {
     Route.post('reset-password', 'AuthController.resetPassword')
   }).prefix('auth')
 
-  // Client routes
+  // Client or Admin routes
   Route.group(() => {
-    Route.put('/clients/:id', 'UsersController.updateClient')
-    Route.get('/clients/:id', 'UsersController.show')
-    Route.resource('pixTransactions', 'PixTransactionsController')
-  }).middleware(['auth', 'is:client'])
+    Route.get('clients/:id/statement', 'PixController.statement')
+    Route.put('users/:id', 'UsersController.update')
+    Route.get('users/:id', 'UsersController.show')
+    Route.resource('pix-transactions', 'PixController').only(['store', 'show'])
+  }).middleware(['auth', 'is:client,admin'])
 
   // Admin routes
   Route.group(() => {
     Route.post('admins', 'UsersController.storeAdmin')
     Route.put('admins/:id', 'UsersController.updateAdmin')
-    Route.get('/clients', 'UsersController.index')
-    Route.delete('/clients/:id', 'UsersController.destroy')
+    Route.get('clients', 'UsersController.index')
+    Route.delete('clients/:id', 'UsersController.destroy')
   }).middleware(['auth', 'is:admin'])
 }).prefix('api/v1')
